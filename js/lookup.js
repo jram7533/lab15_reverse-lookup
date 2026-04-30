55ae520d5b7232636c1b3e5db69ade18d6d3585d
Jack Ramsey <jram7533@gmail.com>
2026-04-30 16:22:17 -0400

    added skeleton for js/lookup.js file

---
+++ js/lookup.js
@@ -0,0 +1,19 @@
+/** lookup.js
+ *      Author:         
+ *      Date:           
+ *      Last revised:   
+ *      Description:    JavaScript for IP Lookup Project
+ *
+ *      - Uses the ipinfo.io API (asynchronously) to look up
+ *        registration information for a user-entered IP 
+ *        address
+ *      - Event listeners for submit and reset buttons
+ *      - Event listener for "Enter" key
+ *      - Field input
+ *          - validate address
+ *      - Uses fetch to query IP Info from ipinfo.io
+ *      - Calls Google Maps API to generate and insert a map
+ *        of the IP location based on the GPS coordinates that
+ *        are returned by ipinfo.io
+ **/
+
